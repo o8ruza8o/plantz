203b29fc446885ef086a1c5f2853e335c4ce5066
Matty G <meawoppl@gmail.com>
2012-06-06 02:37:45 -0700

    Added indicator drawins.  Wooo.

--- tooclassy.py
+++ tooclassy.py
@@ -1,6 +1,12 @@
 import cairo, os
 from math import pi
 
+from pathTools import unpackPath, pointsStats, rootStem
+
+skie_bleu  = (0. /255,    146./ 255, 178./255)
+sun_yeller = (255. / 255, 202. / 255, 61./255)
+
+
 location_list = []
 def push_ctx(ctx):
     location_list.append(ctx.get_current_point())
@@ -18,7 +24,7 @@ class ExpandRules(dict):
         
     def nIterations(self, n):
         if n in self.expansions:
-            return expansions[n]
+            return self.expansions[n]
 
         string = self.startString
         # Todo: seed largest expansion in here . . .
@@ -37,7 +43,7 @@ class RenderRules(dict):
         self.size = 1
         
     def calculatePath(self, instructions):
-        temp_surf = cairo.ImageSurface(cairo.FORMAT_A8, 1, 1)
+        temp_surf = cairo.ImageSurface(cairo.FORMAT_ARGB32, 1, 1)
         ctx = cairo.Context(temp_surf)
 
         ctx.move_to(0,0)
@@ -55,7 +61,7 @@ class RenderRules(dict):
         name, ext = os.path.splitext(filename)
         ext = ext.lower()[1:]
         if   ext == "png":
-            surf = cairo.ImageSurface(cairo.FORMAT_A8, size[0], size[1])
+            surf = cairo.ImageSurface(cairo.FORMAT_ARGB32, size[0], size[1])
         elif ext == "pdf":
             surf =  cairo.PDFSurface(open(filename, "w"), size[0], size[1])
         elif ext == "svg":
@@ -70,51 +76,114 @@ class RenderRules(dict):
         xmin, ymin, xmax, ymax = extents
         x_extent = xmax - xmin
         y_extent = ymax - ymin
-        x_center, y_center = x_extent/2., y_extent/2.
+
+        # Find the centers
+        x_center, y_center = x_extent / 2., y_extent / 2.
+
+        # Compute the scale that makes both axes fit
         scale_factor = 0.9*min(size[0] / x_extent, size[1] / y_extent)
-        x_rextent, y_rextent = scale_factor*x_extent, scale_factor*y_extent
-        x_recenter, y_recenter = (size[0]-x_rextent)/2., (size[1]-y_rextent)/2.
-        x_0, y_0 = x_recenter-scale_factor*xmin, y_recenter-scale_factor*ymin
+
+        # Correct the extents for the rescaling
+        x_rextent, y_rextent = scale_factor * x_extent, scale_factor * y_extent
+        x_recenter, y_recenter = (size[0] - x_rextent) / 2., (size[1] - y_rextent) / 2.
+
+        # Compute new value for recentering
+        x_0, y_0 = x_recenter - scale_factor * xmin, y_recenter - scale_factor * ymin
+
+        # Draw the horizon line
         ctx.move_to(0, y_0)
         ctx.line_to(size[0], y_0)
+
         
-        ctx.set_source_rgb(0.0, 0.0, 0.0)
-        ctx.select_font_face("Helvetica",
-                cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+        # Switch to black, helvetica, normal weight 20pt font
+        ctx.set_source_rgb(0.0, 0.0, 0.)
+        ctx.select_font_face("Helvetica", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
         ctx.set_font_size(20)
-        rule_cnt = 0
-        ctx.move_to(100, size[1]/10 + rule_cnt*40)
-        ctx.show_text("axiom = "+axiom)
-        for r in rules: 
-            rule_cnt+=1
-            ctx.move_to(100, size[1]/10 + rule_cnt*40)
-            ctx.show_text(r+u' --> '+rules[r])
-        draw_cnt = 0
-        for d in draws:
-            draw_cnt+=1
-            ctx.move_to(100, y_0 + draw_cnt*40)
+
+        # Move somewhere to draw the rules
+        ctx.move_to(100, size[1] / 10)
+        ctx.show_text("axiom = " + axiom)
+
+        for rule_cnt, r in enumerate(rules): 
+            ctx.move_to(100, size[1] / 10 + (rule_cnt+1) * 40)
+            ctx.show_text(r+u' --> ' + rules[r])
+
+        for draw_cnt, d in enumerate(draws):
+            ctx.move_to(100, y_0 + (draw_cnt+1)*40)
             if 'rot' in draws[d]:
-                ctx.show_text(d+' == rotate for '+draws[d][11:len(draws[d])-1])
+                ctx.show_text(d + ' == rotate for ' + draws[d][11:len(draws[d])-1])
             elif 'push' in draws[d]:
-                ctx.show_text(d+' == push')
+                ctx.show_text(d + ' == push')
             elif 'pop' in draws[d]:
-                ctx.show_text(d+' == pop')
+                ctx.show_text(d + ' == pop')
             elif 'line' in draws[d]:
-                ctx.show_text(d+' == draw line')
+                ctx.show_text(d + ' == draw line')
             elif 'scale' in draws[d]:
                 if '/' in draws[d]:
-                    ctx.show_text(d+' == scale down by '+draws[d][42:len(draws[d])-1])
+                    ctx.show_text(d + ' == scale down by ' + draws[d][42:len(draws[d])-1])
                 if '*' in draws[d]:
-                    ctx.show_text(d+' == scale up by '+draws[d][42:len(draws[d])-1])
+                    ctx.show_text(d + ' == scale up by ' + draws[d][42:len(draws[d])-1])
+
+        # Move to where drawing the pant will result in centered 
         ctx.translate(x_0, y_0)
+
+        # Scale as computed earlier
         ctx.scale(scale_factor, scale_factor)
+
+
+        # Compute some stats on the system
+        plant_points, plant_segs = unpackPath(path)
+        root_points, stem_points = rootStem(plant_points)
+
+        sx_min, sy_min, sx_max, sy_max, sx_m, sy_m = pointsStats(stem_points)
+        print "Stem values: ", sx_min, sy_min, sx_max, sy_max, sx_m, sy_m
+        rx_min, ry_min, rx_max, ry_max, rx_m, ry_m = pointsStats(root_points)
+        print "Root values: ", rx_min, ry_min, rx_max, ry_max, rx_m, ry_m
+
+        ctx.stroke()
+
+        # Indicator lines
+        ctx.save()
+        
+        ctx.set_line_width(50)
+
+        ctx.set_source_rgb(*skie_bleu)
+        ctx.move_to(rx_min, - 30)
+        ctx.line_to(rx_max, - 30)
+
+        ctx.stroke()
+
+        ctx.set_source_rgb(*sun_yeller)
+        ctx.move_to(sx_min, 30)
+        ctx.line_to(sx_max, 30)
+        ctx.stroke()
+        
+        ctx.move_to(max(sx_max, rx_max), 200)
+        swr = (sx_max - sx_min) / (rx_max - rx_min)
+
+        ctx.set_source_rgb(0.0, 0.0, 0.)
+        ctx.set_font_size(20 / scale_factor * 2)
+        ctx.show_text("Sun/Water:")
+
+        ctx.move_to(max(sx_max, rx_max), 450)
+        ctx.show_text(" %.02f" % swr)
+        ctx.stroke()
+
+        ctx.restore()
+
+
+        # Draw the actual plant
         ctx.append_path(path)
+
+        # Stroke and close
         ctx.stroke()
         surf.finish()
 
         # I am still confused why the png surface is different . . .
         if ext == "png":
             surf.write_to_png(filename)
+
+        return path
 
 if __name__ == "__main__":
     unit = 10
