cf395c1697e4ad6b2d04b9223911015bd871b568
MattyG <meawoppl@gmail.com>
2013-10-07 15:24:31 -0700

    Added starting stuff.

---
+++ quatz.py
@@ -0,0 +1,25 @@
+from math import sqrt
+class Quaterniaon(object):
+    def __init__(w, x, y, z):
+        self.w = w
+        self.xyz = array([x,y,z], dtype=float64)
+
+    @classmethod
+    def normalize():
+        mag = sqrt(w*w + sum(self.xyz**2))
+        self.w /= mag
+        self.xyz /= mag
+
+    @classmethod
+    def conjugate():
+        self.w = self.w
+        self.xyz = -self.xyz
+
+    @classmethod
+    def __mult__(otherQuat):
+        if type(otherQuat) is not "Quaternion":
+            otherQuat = Quaternion(otherQuat, 0, 0, 0)
+
+        self.stuff * otherQuat.stuff
+
+    
